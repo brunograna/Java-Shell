7085f1b8afb3329a1644e596815b92adf43ee9a8
Bruno Garcia <41070501+brunograna@users.noreply.github.com>
2019-10-11 12:15:14 -0300

    Last change

---
+++ src/Shell.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Shell {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String actualPath = System.getProperty("user.dir");
+        List<String> history = new ArrayList<>();
+
+        while (true) {
+            System.setProperty("user.dir", actualPath);
+            System.out.print(actualPath+"/jsh> ");
+            String comando = sc.nextLine();
+            if (comando.equals("")){
+                continue;
+            }else if(comando.equalsIgnoreCase("history")){
+                int count = 0;
+                for (String command: history) {
+                    System.out.println(count+": "+command);
+                    count++;
+                }
+                continue;
+            }else if(comando.startsWith("cd")){
+                history.add(comando);
+                if(comando.indexOf("..") > 0){
+                    String[] pathNow = actualPath.split("/");
+                    pathNow = Arrays.copyOf(pathNow, pathNow.length-1);
+                    actualPath = String.join("/", pathNow);
+                }else{
+                    actualPath = actualPath + comando.substring(3);
+                }
+                System.setProperty("user.dir", actualPath);
+                continue;
+            }
+
+            List<String> commands =  new ArrayList<String>(Arrays.asList(comando.split(" ")));
+//            System.out.println("Run command: "+commands.toString());
+            history.add(comando);
+
+            ProcessBuilder pb = new ProcessBuilder(commands);
+            pb.directory(new File(actualPath));
+            Process process = pb.start();
+
+            // obtain the input stream
+            InputStream is = process.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+
+            // read the output of the process
+            String line;
+            while ( (line = br.readLine()) != null)
+                System.out.println(line);
+            br.close();
+        }
+    }
+}
